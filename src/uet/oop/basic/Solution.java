98f7f822993c8f1d9990c7585ece7ae76901b2a2
NguyenVanTrung1203 <24022475@vnu.edu.vn>
2025-02-26 10:11:35 +0700

    chinh

--- src/uet/oop/basic/Solution.java
+++ src/uet/oop/basic/Solution.java
@@ -1,12 +1,6 @@
 package uet.oop.basic;
 public class Solution {
-    public String printHello() {
-        return "HelloWorld";
-    }
-
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        String message = solution.printHello();
-        System.out.println(message);
+
     }
 }
